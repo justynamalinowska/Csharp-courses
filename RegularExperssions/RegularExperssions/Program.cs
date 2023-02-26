2662402520b7a64bb7cbcb9e195209e3564b9bea
Justyna Malinowska <106835626+justynamalinowska@users.noreply.github.com>
2023-02-26 19:08:17 +0100

    Add files via upload

---
+++ RegularExperssions/RegularExperssions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Xml.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string pattern = @"\d";
+            Regex regex = new Regex(pattern);
+
+            string text = "Hi my number is 456787654 :) ";
+
+            MatchCollection matchCollection = regex.Matches(text);
+
+            Console.WriteLine($"{matchCollection.Count} hits found: \n {text}");
+
+            foreach (Match hit in matchCollection)
+            {
+                GroupCollection group = hit.Groups;
+
+                Console.WriteLine($"{group[0].Value} foun at {group[0].Index}");
+
+            }
+        }
+    }
+}
---
+++ sortowanie_bombelkowe/sortowanie_bombelkowe/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
+            int[] array2 = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
+
+            if (array2[0]>=0 && array2[1]>= 0 && array2[0] <= array2[1] && array2[1] <= array.Length-1 && array.Length < 100)
+            {
+            do
+            {
+                for (int i = array2[0]; i < (array2[1]); i++)
+                {
+                    if (array[i] < array[i + 1])
+                    {
+                        int m = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = m;
+                    }
+                }
+                array2[1] = array2[1] - 1;
+            }
+            while (array2[1] > array2[0]);
+
+            }
+            for (int j = 0; j < array.Length; j++)
+            {
+                Console.Write(array[j] + " ");
+            }
+        }
+    }
+}
